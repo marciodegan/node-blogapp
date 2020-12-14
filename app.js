73ef40f8b6a519b1a1b5e9b96884baa44e4e2b25
marciodegan <marciodegan@yahoo.com>
2020-12-14 18:38:30 -0300

    added middleware

--- app.js
+++ app.js
@@ -23,6 +23,10 @@
         })
     // Public
         app.use(express.static(path.join(__dirname,"public")))
+        app.use((req, res, next) => {
+            console.log("Eu sou um middleware")
+            next()
+        })
 
 
 // Rotas
