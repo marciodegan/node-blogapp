18a86b5d5cfbba2931b9c379291707653435206b
marciodegan <marciodegan@yahoo.com>
2020-12-14 15:42:41 -0300

    added initial routes

--- app.js
+++ app.js
@@ -3,13 +3,22 @@
     const handlebars = require('express-handlebars')
     const bodyParser = require('body-parser')
     const app = express()
+    const admin = require("./routes/admin")
     // const mongoose = require('mongoose')
 
 // Configuracoes
+    // Body Parser
+        app.use(bodyParser.urlencoded({extended: true}))
+        app.use(bodyParser.json())
+    // Handlebars
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars');
+    // Mongoose
+        // em breve
 
 
 // Rotas
-
+    app.use('/admin', admin)
 
 // Outros
 const PORT = 8081
