ce103df6a74c58af270ca7dc6128114c01099e2f
marciodegan <marciodegan@yahoo.com>
2020-12-14 15:31:04 -0300

    initial setup (express/handlebars/body-parser)

---
+++ README.md
@@ -0,0 +1,1 @@
+#### This is a BlogAPP to learn how NodeJS works.
---
+++ app.js
@@ -0,0 +1,18 @@
+// Carregando módulos
+    const express = require('express')
+    const handlebars = require('express-handlebars')
+    const bodyParser = require('body-parser')
+    const app = express()
+    // const mongoose = require('mongoose')
+
+// Configuracoes
+
+
+// Rotas
+
+
+// Outros
+const PORT = 8081
+app.listen(PORT,() => {
+    console.log("Servidor rodando...")
+})
